35fc72692b98eccf7c93e60c0b159ccf5899ce9e
Jason Koo <jalakoo@gmail.com>
2020-03-10 13:06:30 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,42 @@
+# Classifier based Image Sorter
+This app can use an image classifier to detect any number of target labels, then sort those images into 1 of 2 folders.
+
+## Requirements
+- Sign up for [alwaysAI](https://dashboard.alwaysai.co/auth?register=true)
+- Install [alwaysAI tooling](https://dashboard.alwaysai.co/docs/getting_started/development_computer_setup.html)
+
+## Worflow Options
+This app can run on either a desktop/laptop development machine or on an edge device such as a Jetson Nano, Dragonboard, or Raspberry Pi that has docker installed. See the above install instruction link for more details.
+
+## Configuration
+This app makes use of key-values in the `alwaysai.app.json` file for set up and variable assignments:
+
+Key | Value Type | Description
+-----| ---------- | ----------
+scripts | dictionary | Required by alwaysAI's CLI to specify which file to run within a docker container
+models | dictionary | Required by alwaysAI's CLI to determine which CV models to download and make available to the app's docker container
+found_folder | string | Optional location within the app's root folder to put images that have target objects in them. Default is `output_images/found`
+empty_folder | string | Optional location within the app's root folder to put images that had no detected target objects. Default is `output_images/not_found`
+source_folder | string | Optional location within the app's root folder to process images from. Default is `source_images`
+classifier | object | A dictionary with classifier information. See below for expected key-values
+
+Classifier Information:
+
+Key | Value Type | Description
+-----| ---------- | ----------
+model_id | string | The model id as found from [alwaysAI's Model Catalog](https://dashboard.alwaysai.co/model-catalog/models?category=Classification). Note that there should be a duplicate of this string value in the higher level 'models' key-value pair which is used by alwaysAI's CLI tool. This app has not been keyed to use that string as any number of models may be downloaded to an app for other purposes. To add or remove models for download, see [the docs here](https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html).
+confidence_level | float | This is the minimum required confidence level value required by a detected object to be considered found. Each model has a [confidence_level](https://dashboard.alwaysai.co/docs/reference/edgeiq.html#edgeiq.image_classification.ClassificationPrediction) value that's provided with each [classification prediction](https://dashboard.alwaysai.co/docs/reference/edgeiq.html#edgeiq.image_classification.ClassificationResults). Many models will use a float value from 0.0-1.0 with 1.0 = 100%, but some models may have a different value based system, like `squeezenet_v1.1`
+target_labels | array of strings | List of all labels this model should be filtering / looking for. This will usually be a subset of all the available labels this model is capable of detecting. If no labels are specified nothing will be returned, in other words the `found_folder` will be empty after processing
+
+## Running
+Use the alwaysAI CLI to build and start this app:
+Configure (once): `aai app configure`
+Build: `aai app deploy`
+Run: `aai app start`
+
+## Support
+Docs: https://dashboard.alwaysai.co/docs/getting_started/introduction.html
+
+Community Discord: https://discord.gg/R2uM36U
+
+Email: contact@alwaysai.co
---
+++ app.py
@@ -0,0 +1,112 @@
+import cv2
+import edgeiq
+import os
+import json
+import shutil
+
+"""
+Use image classification to sort a batch of images. The
+classification labels can be changed by selecting different models.
+Different images can be used by updating the files in the *source_images/*
+directory. 
+
+NOTE: When developing onto a remote device, removing
+images in the local *source_images/* directory on your dev machine won't remove images 
+from the device. They can be removed using the `aai app shell` command and
+deleting them from the remote's *source_images/* directory.
+
+To change the computer vision model, follow this guide:
+https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
+
+NOTE: This app will auto-detect and use an Intel Movidius Neural Compute stick if
+present. However, not all models can make use of it!
+"""
+
+# Static keys for extracting data from config JSON file
+CONFIG_FILE = 'alwaysai.app.json'
+CLASSIFIER = 'classifier'
+FOUND_FOLDER = 'found_folder'
+EMPTY_FOLDER = 'empty_folder'
+SOURCE_FOLDER = 'source_folder'
+MODEL_ID = 'model_id'
+THRESHOLD = 'confidence_level'
+TARGETS = 'target_labels'
+
+
+def load_json(filepath):
+    '''
+    Convenience to check and load a JSON file
+    '''
+    if os.path.exists(filepath) == False:
+        raise Exception(
+            'app.py: load_json: File at {} does not exist'.format(filepath))
+    with open(filepath) as data:
+        return json.load(data)
+
+
+def main():
+
+    # 1. Load configuration data from the alwaysai.app.json file
+    config = load_json(
+        CONFIG_FILE)
+    found_folder = config.get(FOUND_FOLDER, 'output_images/found')
+    empty_folder = config.get(EMPTY_FOLDER, 'output_images/not_found')
+    source_folder = config.get(SOURCE_FOLDER, 'source_images')
+    classifier_config = config.get(CLASSIFIER, None)
+
+    # 2. Spin up just the classifier
+    model_id = classifier_config.get(MODEL_ID)
+    print('app.py: main(): initializing classifier with model id: {}'.format(
+        model_id))
+    classifier = edgeiq.Classification(model_id)
+    classifier.load(engine=edgeiq.Engine.DNN)
+
+    # 3. Loop through all source images
+    image_paths = sorted(list(edgeiq.list_images(source_folder + '/')))
+    image_count = len(image_paths)
+    print("app.py: main: Checking {} images from '{}' folder ...".format(
+        image_count, SOURCE_FOLDER))
+
+    for image_path in image_paths:
+        # 3a. Load the image to check
+        image_display = cv2.imread(image_path)
+        image = image_display.copy()
+
+        empty_path = image_path.replace(source_folder, empty_folder)
+        confidence = classifier_config.get(THRESHOLD)
+
+        # 3b. Find all objects the classifier is capable of finding
+        results = classifier.classify_image(image, confidence)
+        if len(results.predictions) == 0:
+            # Nothing found with given confidence level - move to the empty folder
+            shutil.move(image_path, empty_path)
+            continue
+        predictions = results.predictions
+
+        # 3c. Filter results by the target labels if specified
+        targets = classifier_config.get(TARGETS, None)
+        if targets is not None:
+            predictions = edgeiq.filter_predictions_by_label(
+                results.predictions, targets)
+            if len(predictions) == 0:
+                # No found labels match taget labels - move to the empty folder
+                shutil.move(image_path, empty_path)
+                continue
+
+        # 3d. At least one target found, move image to found folder
+        _, filename = os.path.split(image_path)
+        print('app.py: main: targets found in {}: {}'.format(
+            filename, predictions))
+        found_path = image_path.replace(source_folder, found_folder)
+        shutil.move(image_path, found_path)
+
+    # Print info to console upon completion
+    print("app.py: main: Completed sorting of {} images".format(image_count))
+    found_images_count = len(
+        list(edgeiq.list_images(found_folder)))
+    print("app.py: main: {} images in the output folder".format(
+        found_images_count))
+
+
+if __name__ == "__main__":
+    main()
